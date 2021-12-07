f939c48289632bf18e147f73a1aeb9e3294e3d1c
ankile <lars.ankile@gmail.com>
2021-12-07 18:17:49 +0100

    fix issue in scheduler

--- utils/schedulers.py
+++ utils/schedulers.py
@@ -1,13 +1,13 @@
 from torch.optim import lr_scheduler
 
 
-def schedule_noop(*args, **kwargs):
+def schedule_noop(*_, **__):
     pass
 
-schedule_noop.step = lambda *args, **kwargs: None
+schedule_noop.step = lambda *_, **__: None
 
 schedulers = {
-    None: lambda opt: schedule_noop,
+    None: lambda _, **__: schedule_noop,
     "ExponentialLR": lambda opt, **kwargs: lr_scheduler.ExponentialLR(opt, **kwargs),
     "MultiStepLR": lambda opt, **kwargs: lr_scheduler.MultiStepLR(opt, **kwargs),
 }
