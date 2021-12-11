a62a9040d4946c732410b9dac116a4e46bc4fc6b
ankile <lars.ankile@gmail.com>
2021-12-11 13:36:16 +0100

    Turn subset into kwarg

--- data/weightnet.py
+++ data/weightnet.py
@@ -18,16 +18,19 @@ def feature_extractor(df, feature_set, n_models, normalization):
     # Get feature inputs
     if feature_set == "":
         raise Exception(
-            "Manual_or_auto_toggle needs to cointain either m or a for input to be non-empty")
+            "Manual_or_auto_toggle needs to cointain either m or a for input to be non-empty"
+        )
 
     inputs_start = "x_acf1" if "m" in feature_set.lower() else "lstm_0"
     inputs_end = "lstm_31" if "a" in feature_set.lower() else "series_length"
 
     inputs = df.loc[:, inputs_start:inputs_end]
 
-    inputs_cat = df.loc[:, ['type', 'period']].astype("category")
-    emb_dims = [(x, min(x // 2, 50))
-                for x in map(lambda y: len(inputs_cat[y].cat.categories), inputs_cat)]
+    inputs_cat = df.loc[:, ["type", "period"]].astype("category")
+    emb_dims = [
+        (x, min(x // 2, 50))
+        for x in map(lambda y: len(inputs_cat[y].cat.categories), inputs_cat)
+    ]
 
     for col in inputs_cat:
         inputs_cat[col] = inputs_cat[col].cat.codes
@@ -39,22 +42,28 @@ def feature_extractor(df, feature_set, n_models, normalization):
 
     # Get actuals
     actuals = df.loc[:, "actuals_0":"actuals_47"].to_numpy()
-    forecasts = forecasts.to_numpy().reshape(
-        (batch_size, n_models, 48)).swapaxes(1, 2)
+    forecasts = forecasts.to_numpy().reshape((batch_size, n_models, 48)).swapaxes(1, 2)
 
     return (inputs_cat, emb_dims), inputs_normalized, forecasts, actuals
 
 
 class M4EnsembleData(Dataset):
-
-    def __init__(self, meta_path, feature_set, n_models, subset, normalize="standard"):
+    def __init__(
+        self, meta_path, feature_set, n_models, subset=None, normalize="standard"
+    ):
         if isinstance(meta_path, pd.DataFrame):
             meta_df = meta_path.copy()
         elif isinstance(meta_path, str):
-            meta_df = pd.read_feather(meta_path).set_index(
-                "m4id").replace(np.nan, 0).loc[subset]
+            meta_df = (
+                pd.read_feather(meta_path)
+                .set_index("m4id")
+                .replace(np.nan, 0)
+                .loc[subset]
+            )
         else:
-            raise Exception("Only pandas DataFrame or path to a feather file legal arguments")
+            raise Exception(
+                "Only pandas DataFrame or path to a feather file legal arguments"
+            )
 
         print(f"Loaded df of shape {meta_df.shape}")
 
@@ -66,8 +75,12 @@ class M4EnsembleData(Dataset):
         self.index = meta_df.index.values
         self.length = meta_df.shape[0]
 
-        (self.cats, emb_dims), self.input, self.forecast, self.actuals = feature_extractor(
-            meta_df, feature_set, n_models, normalize)
+        (
+            (self.cats, emb_dims),
+            self.input,
+            self.forecast,
+            self.actuals,
+        ) = feature_extractor(meta_df, feature_set, n_models, normalize)
 
         self.num_cont = self.input.shape[1]
         self.emb_dims = emb_dims
@@ -76,7 +89,16 @@ class M4EnsembleData(Dataset):
         return self.length
 
     def __getitem__(self, idx):
-        return self.cats[idx], self.input[idx], self.forecast[idx], self.actuals[idx], self.divs[idx], self.n_smape[idx], self.n_mase[idx], self.h[idx]
+        return (
+            self.cats[idx],
+            self.input[idx],
+            self.forecast[idx],
+            self.actuals[idx],
+            self.divs[idx],
+            self.n_smape[idx],
+            self.n_mase[idx],
+            self.h[idx],
+        )
 
 
 def ensemble_loaders(
@@ -99,16 +121,24 @@ def ensemble_loaders(
         train_idxs = split[split.val == False].index
         val_idxs = split[split.val == True].index
 
-    data1 = M4EnsembleData(datapath, feature_set, n_models,
-                           subset=train_idxs, normalize=normalize)
-    loader1 = DataLoader(data1, batch_size=batch_size,
-                         shuffle=training, num_workers=cpus, drop_last=training)
+    data1 = M4EnsembleData(
+        datapath, feature_set, n_models, subset=train_idxs, normalize=normalize
+    )
+    loader1 = DataLoader(
+        data1,
+        batch_size=batch_size,
+        shuffle=training,
+        num_workers=cpus,
+        drop_last=training,
+    )
 
     if val_idxs is not None:
         data2 = M4EnsembleData(
-            datapath, feature_set, n_models, subset=val_idxs, normalize=normalize)
-        loader2 = DataLoader(data2, batch_size=batch_size,
-                             shuffle=False, num_workers=cpus)
+            datapath, feature_set, n_models, subset=val_idxs, normalize=normalize
+        )
+        loader2 = DataLoader(
+            data2, batch_size=batch_size, shuffle=False, num_workers=cpus
+        )
 
         return loader1, loader2, data1.emb_dims, data1.num_cont, data1.length
 
@@ -129,12 +159,15 @@ def ensemble_loaders_kfold(
     print(f"CPU count: {cpus}")
 
     data1 = M4EnsembleData(data, feature_set, n_models, normalize=normalize)
-    loader1 = DataLoader(data1, batch_size=batch_size,
-                         shuffle=training, num_workers=cpus, drop_last=training)
+    loader1 = DataLoader(
+        data1,
+        batch_size=batch_size,
+        shuffle=training,
+        num_workers=cpus,
+        drop_last=training,
+    )
 
-    data2 = M4EnsembleData(
-        val, feature_set, n_models, normalize=normalize)
-    loader2 = DataLoader(data2, batch_size=batch_size,
-                         shuffle=False, num_workers=cpus)
+    data2 = M4EnsembleData(val, feature_set, n_models, normalize=normalize)
+    loader2 = DataLoader(data2, batch_size=batch_size, shuffle=False, num_workers=cpus)
 
     return loader1, loader2, data1.emb_dims, data1.num_cont, data1.length
