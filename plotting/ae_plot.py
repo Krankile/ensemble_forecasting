20d02d355ac69939204f4cf1b8d9d5e798ce3217
ankile <lars.ankile@gmail.com>
2021-12-14 13:01:17 +0100

    Set labelsize

--- plotting/ae_plot.py
+++ plotting/ae_plot.py
@@ -20,6 +20,7 @@ def plot_examples(figurepath,
         ax.xaxis.set_major_locator(plt.MaxNLocator(3))
         ax.plot(d1, label=f'Original', color=main)
         ax.plot(d2, label=f'Reconstructed', color=main2)
+        ax.tick_params(labelsize=14)
         ax.legend(fontsize=14)
 
     data2 = pad_packed_sequence(model(example_packed, lens), batch_first=True, total_length=conf.maxlen)[0].detach().cpu().numpy()
@@ -38,8 +39,6 @@ def plot_examples(figurepath,
 
     fig.tight_layout()
     fig.subplots_adjust(top=0.92)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
 
     plt.savefig(figurepath)
     if not show:
