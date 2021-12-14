ef9145ea8714264e712ca17152b96e43f1eb4cdb
ankile <lars.ankile@gmail.com>
2021-12-14 13:33:02 +0100

    Even better ae plot

--- plotting/ae_plot.py
+++ plotting/ae_plot.py
@@ -9,26 +9,28 @@ def plot_examples(figurepath,
                   example_packed,
                   model,
                   lens,
-                  epoch,
                   conf,
                   rows=3,
                   cols=3,
                   size=(16, 8),
                   show=False,):
 
+    prisize = 16
+    secsize = 14
+
     def plot(ax, d1, d2):
         ax.xaxis.set_major_locator(plt.MaxNLocator(3))
         ax.plot(d1, label=f'Original', color=main)
         ax.plot(d2, label=f'Reconstructed', color=main2)
-        ax.tick_params(labelsize=14)
-        ax.legend(fontsize=14)
+        ax.tick_params(labelsize=secsize)
+        ax.legend(fontsize=secsize)
 
     data2 = pad_packed_sequence(model(example_packed, lens), batch_first=True, total_length=conf.maxlen)[0].detach().cpu().numpy()
 
     figs = np.array(range(rows*cols)).reshape((rows, cols))
     fig, axs = plt.subplots(nrows=rows, ncols=cols, figsize=size)
 
-    fig.suptitle(f"Autoencoder reconstructions on val data", size=16)
+    fig.suptitle(f"Autoencoder reconstructions on val data", size=prisize)
 
     idx = 0
 
@@ -36,6 +38,12 @@ def plot_examples(figurepath,
         for ax, r in zip(rfig, rdat):
             plot(ax, data1[r][:lens[r]], data2[r][:lens[r]])
             idx += 1
+
+    for i in range(rows):
+        axs[0][i].set_ylabel("Standardized value", fontize=secsize)
+
+    for i in range(cols):
+        axs[-1][i].set_ylabel("Timestep", fontize=secsize)
 
     fig.tight_layout()
     fig.subplots_adjust(top=0.92)
