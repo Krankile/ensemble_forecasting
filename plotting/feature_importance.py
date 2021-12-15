17945d0fb749141ad046fdf070ea68ed4a457145
ankile <lars.ankile@gmail.com>
2021-12-15 16:07:03 +0100

    Added error bars

--- plotting/feature_importance.py
+++ plotting/feature_importance.py
@@ -5,22 +5,35 @@ from .colors import main, main2
 from . import plot_standards as ps
 
 
-def plot_feature_importance(importances, *, savepath=None, show=True, normalize=False):
+def plot_feature_importance(importances, *, savepath=None, useerror=None, show=True, normalize=False, figheight=12):
     mean_results = {k: np.mean(v) for k, v in importances.items()}
     sorted_results = sorted(mean_results.items(), key=lambda x: x[1], reverse=True)
     lab, vals = zip(*sorted_results)
     vals = np.array(vals)
+    
 
     if normalize:
         vals /= vals.sum()
 
-    plt.figure(figsize=(ps.figwidth, 12))
+    lstm_err = None
+    stat_err = None
+    if useerror is not None:
+        if useerror == "sd":
+            err = {k: np.std(v) for k, v in importances.items()}
+        elif useerror == "se":
+            err = {k: np.std(v) / np.sqrt(len(v)) for k, v in importances.items()}
+
+        lstm_err = [v for k, v in err if "lstm" in k]
+        stat_err = [v for k, v in err if "lstm" not in k]
+
+
+    plt.figure(figsize=(ps.figwidth, figheight))
 
     zero = np.zeros(len(lab))
     plt.barh(lab, zero, color=main)
 
-    plt.barh(*zip(*filter(lambda x: "lstm" in x[0], zip(lab, vals))), alpha=1, color=main, label="LSTM AE features")
-    plt.barh(*zip(*filter(lambda x: "lstm" not in x[0], zip(lab, vals))), alpha=1, color=main2, label="Statistical features")
+    plt.barh(*zip(*filter(lambda x: "lstm" in x[0], zip(lab, vals))), alpha=1, color=main, xerr=lstm_err, label="LSTM AE features")
+    plt.barh(*zip(*filter(lambda x: "lstm" not in x[0], zip(lab, vals))), alpha=1, color=main2, xerr=stat_err, label="Statistical features")
     
     plt.xlabel("OWA loss relative to baseline", fontsize=ps.textsize)
     plt.yticks(fontsize=10)
