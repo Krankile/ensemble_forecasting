95d326dd74ab393104941badf000e74981500d98
ankile <lars.ankile@gmail.com>
2021-12-15 22:30:02 +0100

    Add more plot settings

--- plotting/feature_importance.py
+++ plotting/feature_importance.py
@@ -5,7 +5,7 @@ from .colors import main, main2
 from . import plot_standards as ps
 
 
-def plot_feature_importance(importances, *, savepath=None, useerror=None, show=True, normalize=False, figheight=12):
+def plot_feature_importance(importances, *, savepath=None, useerror=None, show=True, normalize=False, figheight=12, yticksize=10):
     mean_results = {k: np.mean(v) for k, v in importances.items()}
     sorted_results = sorted(mean_results.items(), key=lambda x: x[1], reverse=True)
     lab, vals = zip(*sorted_results)
@@ -36,7 +36,7 @@ def plot_feature_importance(importances, *, savepath=None, useerror=None, show=T
     plt.barh(*zip(*filter(lambda x: "lstm" not in x[0], zip(lab, vals))), alpha=1, color=main2, xerr=stat_err, label="Statistical features")
     
     plt.xlabel("OWA loss relative to baseline", fontsize=ps.textsize)
-    plt.yticks(fontsize=10)
+    plt.yticks(fontsize=yticksize)
     plt.xticks(fontsize=ps.ticksize)
     
     plt.title("Feature importance calculations across all 76 features for validation data", fontsize=ps.textsize)
